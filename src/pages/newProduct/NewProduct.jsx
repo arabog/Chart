e672018744b7d601d016137b72c3848b3cc019b1
iCode_Rx <arababs2015@gmail.com>
2021-10-01 08:50:14 +0100

    new product page added

--- src/pages/newProduct/NewProduct.jsx
+++ src/pages/newProduct/NewProduct.jsx
@@ -1,9 +1,39 @@
 import "./newProduct.css"
 
+
 export default function NewProduct() {
+
+          
           return (
                     <div className="newProduct">
-                              New Products
+                              <h1 className="addProductTitle">New Product</h1>
+
+                              <form className="addProductForm">
+                                        <div className="addProductItem">
+                                                  <label htmlFor="file">Image</label>
+                                                  <input type="file"  id="file" />
+                                        </div>
+
+                                        <div className="addProductItem">
+                                                  <label htmlFor=" name">Name</label>
+                                                  <input type="text"  id="name" placeholder="Apple Airpods" />
+                                        </div>
+
+                                        <div className="addProductItem">
+                                                  <label htmlFor=" name">Name</label>
+                                                  <input type="text"  id="name"  placeholder="123" />
+                                        </div>
+
+                                        <div className="addProductItem">
+                                                  <label htmlFor=" active">Active</label>
+                                                  <select name="active" id="active">
+                                                            <option value="yes"> Yes </option>
+                                                            <option value="no"> No </option>
+                                                  </select>
+                                        </div>
+
+                                        <button className="addProductButton">Create New Product</button>
+                              </form>
                     </div>
           )
 }
